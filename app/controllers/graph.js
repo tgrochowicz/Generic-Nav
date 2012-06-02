617b170a9c2fbf455ecb145f214b5b0ef3254fb6
Michael Pratt <urthen@gmail.com>
2012-06-02 12:25:52 -0500

    more automatical graphing

--- app/controllers/graph.js
+++ app/controllers/graph.js
@@ -1,5 +1,5 @@
 var mapping = require('../mapping')
 
 exports.go = function(req, res) {
-	res.render('graph', {'graph': mapping.graph}); // TODO: Use the word 'graph' more
+	res.render('graph', {'graph': mapping.graph, "floors": mapping.floors}); // TODO: Use the word 'graph' more
 }
--- app/mapping.js
+++ app/mapping.js
@@ -1,3 +1,49 @@
-var graph = require("../assets/graph");
+var nodes = require("../assets/graph"),
+	graph = {},
+	floors = [];
+
+for (var floor in nodes.floors) {
+	floor = parseInt(floor);
+	floors[floor] = nodes.floors[floor] //I am floored by this code
+}
+
+for (var endpoint in nodes.endpoints) {
+	var node = nodes.endpoints[endpoint];
+	graph[endpoint] = {
+		"name": node.name,
+		"pos": node.pos,
+		"connections": {}
+	};
+}
+
+for (var junction in nodes.junctions) {
+	var node = nodes.junctions[junction];
+	graph[junction] = {
+		"name": junction,
+		"pos": node.pos,
+		"connections": {}
+	}
+	for (var name in node.connections) {
+		var endpoint = graph[name];
+		graph[junction].connections[name] = node.connections[name][0];
+		endpoint.connections[junction] = node.connections[name][1];
+	}
+}
+
+for (var elevator in nodes.elevators) {
+	var node = nodes.elevators[elevator];
+	graph[elevator] = {
+		"name": node.name,
+		"pos": node.pos,
+		"connections": {}
+	}
+	for (var i in node.exits) {
+		var exit = node.exits[i],
+			junction = graph[exit];
+		graph[elevator].connections[exit] = "Exit the elevator at the " + floors[i];
+		junction.connections[elevator] = "Enter the " + node.name;
+	}
+}
 
 exports.graph = graph;
+exports.floors = floors;

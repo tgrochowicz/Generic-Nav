a04bbfbf0b0b7a06c49f2d4f66417ad7aa05b598
Tom Grochowicz <tom.grochowicz@bazaarvoice.com>
2012-06-02 14:29:34 -0500

    Dynamic "You Are Here" positioning

---
+++ static/js/node.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	var map = $('#map').offset();
+	var location = $('#location');
+
+	var top = map.top - 57 / 2;
+	var left = map.left - 62 / 2;
+
+	var offset = location.offset()
+	location.offset({'top':offset.top + top, 'left':offset.left + left});
+});
+

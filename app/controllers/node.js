a0b611f2e792654117955d1dfcf93837e3b05635
Tom Grochowicz <tom.grochowicz@bazaarvoice.com>
2012-06-02 13:07:17 -0500

    Floor Images on Node Page

--- app/controllers/node.js
+++ app/controllers/node.js
@@ -1,6 +1,7 @@
 var mapping = require('../mapping')
 
 exports.go = function(req, res) {
-	var params = {'node': mapping.graph[req.params.nodeid] }
+	var node = mapping.graph[req.params.nodeid]
+	var params = {'node': node , 'floor': mapping.floors[node.pos[2]] }
 	res.render('node', params);
 }

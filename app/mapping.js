c4269b2f99d01ee8588a98fe4b8b9d319f9eca8e
Michael Pratt <urthen@gmail.com>
2012-06-02 19:29:49 -0500

    refactoring mapping.js slightly

--- app/mapping.js
+++ app/mapping.js
@@ -1,4 +1,4 @@
-var nodes = require("../static/js/graph"),
+var nodes = undefined,
 	graph = {},
 	floors = [];
 
@@ -11,69 +11,79 @@ function calcDist(a, b) {
 	return Math.sqrt(x * x + y * y)
 }
 
-for (var floor in nodes.floors) {
-	floor = parseInt(floor);
-	floors[floor] = nodes.floors[floor] //I am floored by this code
+function loadNodes() {
+	nodes = require("../static/js/graph")
+	exports.nodes = nodes
 }
 
-for (var endpoint in nodes.endpoints) {
-	var node = nodes.endpoints[endpoint];
-	graph[endpoint] = {
-		"id": endpoint,
-		"name": node.name,
-		"pos": node.pos,
-		"type": "endpoint",
-		"connections": {}
-	};
-}
+function parseNodes() {
 
-for (var junction in nodes.junctions) {
-	var node = nodes.junctions[junction];
-	graph[junction] = {
-		"id": junction,
-		"name": junction,
-		"pos": node.pos,
-		"type": "junction",
-		"connections": {}
+	for (var floor in nodes.floors) {
+		floor = parseInt(floor);
+		floors[floor] = nodes.floors[floor] //I am floored by this code
 	}
-	for (var name in node.connections) {
-		var endpoint = graph[name];
 
-		if (endpoint) {
-			dist = calcDist(node, endpoint);
-			graph[junction].connections[name] = {
-				"desc": node.connections[name][0],
-				"dist": dist
+	for (var endpoint in nodes.endpoints) {
+		var node = nodes.endpoints[endpoint];
+		graph[endpoint] = {
+			"id": endpoint,
+			"name": node.name,
+			"pos": node.pos,
+			"type": "endpoint",
+			"connections": {}
+		};
+	}
+
+	for (var junction in nodes.junctions) {
+		var node = nodes.junctions[junction];
+		graph[junction] = {
+			"id": junction,
+			"name": junction,
+			"pos": node.pos,
+			"type": "junction",
+			"connections": {}
+		}
+		for (var name in node.connections) {
+			var endpoint = graph[name];
+
+			if (endpoint) {
+				dist = calcDist(node, endpoint);
+				graph[junction].connections[name] = {
+					"desc": node.connections[name][0],
+					"dist": dist
+				}
+				endpoint.connections[junction] = {
+					"desc": node.connections[name][1],
+					"dist": dist
+				}
 			}
-			endpoint.connections[junction] = {
-				"desc": node.connections[name][1],
-				"dist": dist
+		}
+	}
+
+	for (var elevator in nodes.elevators) {
+		var node = nodes.elevators[elevator];
+		graph[elevator] = {
+			"id": elevator,
+			"name": node.name,
+			"pos": node.pos,
+			"type": "elevator",
+			"connections": {}
+		}
+		for (var i in node.exits) {
+			var exit = node.exits[i],
+				junction = graph[exit];
+			graph[elevator].connections[exit] = {
+				"desc": "Exit the elevator at the " + floors[i].description,
+				"dist": calcDist(graph[elevator], junction) + 0.5
+			}
+			junction.connections[elevator] = {
+				"desc": "Enter the " + node.name,
+				"dist": calcDist(graph[elevator], junction) + 0.5
 			}
 		}
 	}
-}
-
-for (var elevator in nodes.elevators) {
-	var node = nodes.elevators[elevator];
-	graph[elevator] = {
-		"id": elevator,
-		"name": node.name,
-		"pos": node.pos,
-		"type": "elevator",
-		"connections": {}
-	}
-	for (var i in node.exits) {
-		var exit = node.exits[i],
-			junction = graph[exit];
-		graph[elevator].connections[exit] = {
-			"desc": "Exit the elevator at the " + floors[i].description,
-			"dist": calcDist(graph[elevator], junction) + 0.5
-		}
-		junction.connections[elevator] = {
-			"desc": "Enter the " + node.name,
-			"dist": calcDist(graph[elevator], junction) + 0.5
-		}
-	}
+	exports.graph = graph;
+	exports.floors = floors;
 }
 
 function addToPath(path, name, connection) {
@@ -211,7 +221,7 @@ function getRoute (from, to) {
 	return bestPath;
 }
 
-exports.nodes = nodes;
-exports.graph = graph;
-exports.floors = floors;
+loadNodes();
+parseNodes();
+
 exports.getRoute = getRoute;

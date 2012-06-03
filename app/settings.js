f76d71b15db01d43df1f6841e06a4b67b2def927
Michael Pratt <urthen@gmail.com>
2012-06-03 08:26:39 -0500

    adding kiosk support

--- app/settings.js
+++ app/settings.js
@@ -8,6 +8,8 @@ exports.boot = function (app) {
 		app.set("view options", {"layout": false});
 		app.use(express.bodyParser());
 		app.use(express.methodOverride());
+		app.use(express.cookieParser());
+		app.use(express.session({ secret: 'boxakoala'}));
 		app.use(app.router);
 		app.use(express.static(__dirname + "/../static/"));
 		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));

b4612302b55dbb227f41d25ecd3217855276c657
Michael Pratt <urthen@gmail.com>
2012-06-03 00:06:47 -0500

    fixing a few outstanding bugs

--- static/js/node.js
+++ static/js/node.js
@@ -1,13 +1,28 @@
 $(function(){
 
-	var map = $('#map').offset();
-	var location = $('#location');
+	var $canvas = $('#mapcanvas'),
+		pos = JSON.parse($canvas.attr('data-pos')),
+		filename = "/images/" + $canvas.attr('data-bg'),
+		context,
+		bg = new Image(),
+		here = new Image(),
+		scale = 1; //Make this dynamic at some point...
 
-	var top = map.top - 57 / 2;
-	var left = map.left - 62 / 2;
+	bg.onload = function() {
+		var canvas = document.getElementById('mapcanvas');
+		canvas.width = this.width * scale;
+		canvas.height = this.height * scale;
+		context = canvas.getContext('2d');
+		context.scale(scale, scale);
+		context.drawImage(bg, 0, 0);
+		here.src = "/images/here.png";
+	};
 
-	var offset = location.offset()
-	location.offset({'top':offset.top + top, 'left':offset.left + left});
+	here.onload = function() {
+		context.drawImage(here, pos[0] - this.width / 2, pos[1] - this.height)
+	}
+
+	bg.src = filename;
 
 	$('#find').bind('click', function()
 	{

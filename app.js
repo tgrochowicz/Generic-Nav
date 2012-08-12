7bb31fc4f4aca4fd7eab95ca1b60ec5888ef1fb2
Tom Grochowicz <tom.grochowicz@bazaarvoice.com>
2012-08-11 19:59:45 -0500

    Support for Express 3.x

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require("express");
 
 // Setup Express Application
-var app = express.createServer();
+var app = express();
 require("./app/settings").boot(app);
 require("./app/router").setup(app);
-
-// Good to go, brah
-app.listen(10101, function () {
-	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.listen(10101);
